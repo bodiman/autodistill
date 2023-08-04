805d2de60c426b1382f9e81f599a45fd85290c73
Bodi Szabo <bodszab@nuevaschool.org>
2023-08-03 21:11:22 -0700

    reorganizing to fix another circular import

--- autodistill/core/__init__.py
+++ autodistill/core/__init__.py
@@ -1,3 +1,3 @@
+from autodistill.corebase.ontology import Ontology
 from autodistill.core.base_model import BaseModel
-from autodistill.core.ontology import Ontology
 from autodistill.core.target_model import TargetModel
