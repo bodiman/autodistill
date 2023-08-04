17632c6d80659458ca883f86cefebccad500a426
Bodi Szabo <bodszab@nuevaschool.org>
2023-08-03 21:26:21 -0700

    bug fix

--- autodistill/detection/__init__.py
+++ autodistill/detection/__init__.py
@@ -1,4 +1,7 @@
-from autodistill.detectionbase.detection_base_model import DetectionBaseModel
+from autodistill.detection import detection_ontology
+from autodistill.detectionbase import detection_base_model
+
+from autodistill.detectionbase.detection_base_model import DetectionBase
 from autodistill.detection.caption_ontology import CaptionOntology
 from autodistill.detection.detection_ontology import DetectionOntology
 from autodistill.detection.detection_target_model import DetectionTargetModel
